b64e8bb0a9de3a510e8e99afbb683421700f4e60
sujata108btcse21 <sujata108btcse21@igdtuw.ac.in>
2024-04-15 22:53:33 +0530

    Weather App

---
+++ weather-app/src/api/api.js
@@ -0,0 +1,11 @@
+const apiKey = '8bdbbb004fdee44e0f6a36e765746654';
+
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+
+export default getWeather;
